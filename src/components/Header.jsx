6ba030cb1e441bf641dc2ae0435c29a1b7893da7
Angelo Betancourt <angelo@MacBook-Pro-de-Angelo.local>
2024-03-20 17:44:15 -0400

    Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from '../icons/Logo'
+import '../styles/Header.css'
+
+function Header () {
+  return (
+    <header>
+      <div>
+        <Logo />
+      </div>
+    </header>
+  )
+}
+
+export default Header
