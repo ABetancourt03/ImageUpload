4f738739db69071d8abfeab09937e5f5e1a37103
Angelo <angelo@MacBook-Pro-de-Angelo.local>
2024-03-26 14:21:34 -0400

    Add Dark Mode to Header and Body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,41 @@
+import { useState, useEffect } from 'react'
 import Logo from '../icons/Logo'
 import Moon from '../icons/Moon'
+import Sun from '../icons/Sun'
 import '../styles/Header.css'
 
 function Header () {
+  const [theme, setTheme] = useState('light')
+  const $body = document.body
+
+  const handleThemeChange = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+      localStorage.setItem('theme', 'dark')
+      $body.classList.add('dark')
+      $body.classList.remove('light')
+    } else if (theme === 'dark') {
+      setTheme('light')
+      localStorage.setItem('theme', 'light')
+      $body.classList.add('light')
+      $body.classList.remove('dark')
+    }
+  }
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme')
+    $body.classList.add(localTheme)
+    setTheme(localTheme)
+  }, [theme, $body])
+
   return (
-    <header>
+    <header className={theme}>
       <Logo />
 
-      <button className='theme-button'>
-        <Moon />
+      <button onClick={handleThemeChange} className={`theme-button ${theme}`}>
+        {
+          theme === 'light' ? <Moon /> : <Sun />
+        }
       </button>
     </header>
   )
